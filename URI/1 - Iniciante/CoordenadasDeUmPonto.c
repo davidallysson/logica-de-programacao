f8f0c34c3816ee3f0bd3b64167e154febe829dd9
David Allysson <davidmoreirainformatica@gmail.com>
2017-09-04 17:50:54 -0300

    questoes do URI e do UVA

--- URI/1 - Iniciante/CoordenadasDeUmPonto.c
+++ URI/1 - Iniciante/CoordenadasDeUmPonto.c
@@ -3,20 +3,20 @@
 int main() {
   float x, y;
   scanf("%f %f", &x, &y);
-  if (x == 0.0 && y == 0.0) {
+  if (x == 0.0f && y == 0.0f) {
     printf("Origem\n");
-  } else if (x == 0.0) {
-    printf("Eixo X\n");
-  } else if (y == 0.0) {
+  } else if (x == 0.0f) {
     printf("Eixo Y\n");
-  } else if (x > 0.0 && y > 0.0) {
+  } else if (y == 0.0f) {
+    printf("Eixo X\n");
+  } else if (x > 0.0f && y > 0.0f) {
     printf("Q1\n");
-  } else if (x < 0.0 && y > 0.0) {
+  } else if (x < 0.0f && y > 0.0f) {
     printf("Q2\n");
-  } else if (x < 0.0 && y < 0.0) {
+  } else if (x < 0.0f && y < 0.0f) {
     printf("Q3\n");
-  } else if (x > 0.0 && y < 0.0) {
+  } else if (x > 0.0f && y < 0.0f) {
     printf("Q4\n");
-  } 
+  }
   return 0;
 }
