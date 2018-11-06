2a94a3c5834722c07d98f878f150fdd9e79bfc63
Rodrigo Santos <rodrigosferreira91@gmail.com>
2018-11-06 09:52:54 -0200

    Adiciona sequenciais3 em python3

---
+++ 1 - Sequenciais/sequenciais3.py
@@ -0,0 +1,5 @@
+#Faça um programa que peça dois números e imprima a soma
+
+primeiro_numero = input("Digite um número: ")
+segundo_numero = input("Digite outro número: ")
+print("A soma dos dois números informados é: ", int(primeiro_numero) + int(segundo_numero))
